e9f2585685e4864a6e35db2f0695f95414ba08a3
Jihad Yusoph <jihadyusoph.09@gmail.com>
2022-06-10 17:58:09 +0800

    Softeng UI

---
+++ application/views/EditEmployee.php
@@ -0,0 +1,82 @@
+<div class="section">
+        <div class="container ">
+            <div class="row mt-5 justify-content-center">
+                <div class="card ">
+                        <div class="card-front mt-5 ">
+                        <h1 class="text-center">Edit Employee</h1>
+                        <hr style="color: #1f2839;">
+                        
+                        <div class="mx-5 mb-3 mt-5">
+                            <label for="examplefirstname1" class="form-label">Full Name</label>
+                            <input type="text" class="form-control" id="exampleInputlastname1" aria-describedby="emailHelp">
+
+                        </div>
+                        <div class="mx-5 mb-3">
+                            <label for="examplefirstname1" class="form-label">Username</label>
+                            <input type="text" class="form-control" id="exampleInputlastname1" aria-describedby="emailHelp">
+                        </div>
+                        <div class="mx-5 mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        </div>
+                        <div class="mx-5 mb-3">
+                            <label for="exampleInputPassword1" class="form-label">New Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <div class="d-grid gap-2 mt-5 d-md-flex justify-content-md-center">
+                            <button class="btn btn-lg mt-5 " type="button">Update</button>
+                            <button class="btn btn-lg mt-5" type="button">Cancel</button>
+                          </div>
+                    </div>
+                    
+                </div>
+                </div>
+                
+        
+</div>
+
+
+
+
+
+<style>
+body{
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: 300;
+    font-size: 20px;
+    background-color: #1f2839;
+    overflow-x: hidden;
+}
+
+
+p {
+font-weight: 500;
+font-size: 14px;
+line-height: 1.7;
+}
+
+.form-label{
+color: #1f2839;
+}
+.card h1{
+padding: 0 20px;
+font-weight: bold;
+color: #1f2839;
+}
+
+.card {
+position: relative;
+width: 900px;
+max-width: 100%;
+height: 700px;
+background-color: #f5f5ef;
+margin: 25px;
+}
+.btn{
+color: #1f2839;
+background-color: #b69d74;
+}
+
+
+
+</style>
